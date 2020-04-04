c8bc5eda81271ebacf5db5bc3627a06bfd338a49
korostelevm <korostelevm@gmail.com>
2020-04-04 12:45:36 -0400

    asdf

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,26 @@
 #! /usr/bin/env bash
-set -ex
-sha_8=$(git rev-parse HEAD | cut -c1-8)
-app_name=${PWD##*/}-xxxxxx
+set -e
+version=$(git rev-parse HEAD | cut -c1-8)
+version=xxxxxxx
+app_name=${PWD##*/}
 bucket=$1
+
+
+echo "
+Deploying - 
+    bucket: ${bucket}
+    version: ${version}
+    app_name: ${app_name}
+
+    "
+echo -n "you down with that? (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo Deploying ${app_name}-${version}
+else
+    echo Aborted
+    exit 0
+fi
 
 npm run build:microfrontend
 
@@ -10,9 +28,9 @@ aws cloudformation deploy --template-file template-static-infra.yaml --stack-nam
 APIDomainName=$(aws cloudformation describe-stack-resources --stack-name ${app_name}-static-infra --logical-resource-id APIDomainName --query 'StackResources[].{name:PhysicalResourceId}' --output text)
 
 
-sam package --template-file template-api.yaml --s3-bucket ${bucket} --s3-prefix ${app_name}-api --output-template-file template-api-built.yaml
-sam deploy --template-file template-api-built.yaml --stack-name ${stack_name} --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+sam package --template-file template-api.yaml --s3-bucket ${bucket} --s3-prefix ${app_name}-${version} --output-template-file template-api-built.yaml
+sam deploy --template-file template-api-built.yaml --stack-name ${app_name}-${version}-api --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
 
 # launch/update zdt switch stack wand point to api from service
-RestApiId=$(aws cloudformation describe-stack-resources --stack-name ${app_name}-api --logical-resource-id ServiceApi --query 'StackResources[].{name:PhysicalResourceId}' --output text)
-aws cloudformation deploy --template-file ${app_name}-version-pointer.template --stack-name ${app_name}-version-pointer --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides RestApiId=${RestApiId} APIDomainName=${APIDomainName} CreateMapping=true
+RestApiId=$(aws cloudformation describe-stack-resources --stack-name ${app_name}-${version}-api --logical-resource-id ServiceApi --query 'StackResources[].{name:PhysicalResourceId}' --output text)
+aws cloudformation deploy --template-file template-version-pointer.yaml --stack-name ${app_name}-version-pointer --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides RestApiId=${RestApiId} APIDomainName=${APIDomainName} CreateMapping=true
